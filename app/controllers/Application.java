cb2f4710e864ebc267aa1460fa00228b04fc8419
Sam Khawse <sam.khawse@gmail.com>
2011-08-08 19:17:25 -0500

    The model layer is ready

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,7 +10,11 @@ import models.*;
 public class Application extends Controller {
 
     public static void index() {
-        render();
+    	 Post frontPost = Post.find("order by postedAt desc").first();
+         List<Post> olderPosts = Post.find(
+             "order by postedAt desc"
+         ).from(1).fetch(10);
+         render(frontPost, olderPosts);
     }
 
 }
