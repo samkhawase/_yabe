fe01339f5a900a120ae4f481f3a47d5e13e583df
yantramanav <sam.khawse@gmail.com>
2011-08-10 14:45:34 -0500

    Completed comments section, with JQuery, and flash message

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import play.*;
+import play.data.validation.*;
 import play.mvc.*;
 
 import java.util.*;
@@ -9,6 +10,12 @@ import models.*;
 
 public class Application extends Controller {
 
+    @Before
+    static void addDefaults() {
+        renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
+        renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
+    }
+    
     public static void index() {
     	 Post frontPost = Post.find("order by postedAt desc").first();
          List<Post> olderPosts = Post.find(
@@ -16,10 +23,19 @@ public class Application extends Controller {
          ).from(1).fetch(10);
          render(frontPost, olderPosts);
     }
-
-    @Before
-    static void addDefaults() {
-        renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
-        renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
+    
+    public static void show(Long id) {
+        Post post = Post.findById(id);
+        render(post);
+    }
+    
+    public static void postComment(Long postId, @Required String author, @Required String content) {
+        Post post = Post.findById(postId);
+        if (validation.hasErrors()) {
+            render("Application/show.html", post);
+        }
+        post.addComment(author, content);
+        flash.success("Thanks for posting %s", author);
+        show(postId);
     }
 }
--- app/models/Post.java
+++ app/models/Post.java
@@ -34,4 +34,11 @@ public class Post extends Model {
 	    return this;
 	}
 
+	public Post previous() {
+	    return Post.find("postedAt < ? order by postedAt desc", postedAt).first();
+	}
+	 
+	public Post next() {
+	    return Post.find("postedAt > ? order by postedAt asc", postedAt).first();
+	}
 }
