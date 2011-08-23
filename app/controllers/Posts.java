ea5084d9ae4d81380c844bcdf0ccf2e9f1798286
yantramanav <sam.khawse@gmail.com>
2011-08-23 15:44:52 -0500

    Security module - Yikes forgot files to add some files

--- app/controllers/Posts.java
+++ app/controllers/Posts.java
@@ -1,8 +1,11 @@
 package controllers;
 
+import java.net.SecureCacheResponse;
+
 import play.*;
 import play.mvc.*;
 
+@With(Secure.class)
 public class Posts extends CRUD{
 
 }
--- app/controllers/Users.java
+++ app/controllers/Users.java
@@ -2,6 +2,7 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
- 
+
+@With(Secure.class)
 public class Users extends CRUD {    
 }
