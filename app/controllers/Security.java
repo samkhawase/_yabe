955cfdaac5ae58275eb39afa563f4dc98ccb4aea
yantramanav <sam.khawse@gmail.com>
2011-08-23 15:42:26 -0500

    Security module, b4 refactoring Admin\CRUD area

---
+++ app/controllers/Security.java
@@ -0,0 +1,11 @@
+package controllers;
+
+import models.*;
+
+public class Security extends Secure.Security {
+	
+    static boolean authenticate(String username, String password) {
+    	return User.connect(username, password) != null;
+    }
+    
+}
